4a7af4fa2969cf91de2998fc84a6fe377a2a9721
jenish0185 <127487076+jenish0185@users.noreply.github.com>
2023-11-23 10:28:20 +0545

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,17 @@
+//Task 1.  Write a program to get the following output.
+//Hey there,
+//I am “some data”! (assign a variable and print the variable data)
+
+
+public class task1 {
+    public static void main(String[] args) {
+       
+        String name= "Jenish Shrestha" ;
+
+        System.out.println("Hey there!");
+        System.out.println("I am "+name);
+
+    }
+}
+    
+
