c442b1889b1d898ccd32376ce4f616e0e2815c84
jenish0185 <127487076+jenish0185@users.noreply.github.com>
2023-11-25 19:46:15 +0545

    Add files via upload

---
+++ task2.java
@@ -0,0 +1,17 @@
+//Task 2. Write a program to print the difference and product of numbers 45 and 32. 
+
+public class task2 {
+    public static void main(String[] args) {
+        int firstNumber = 45, secondNumber = 32;
+        int difference, product;
+
+        difference = firstNumber-secondNumber;
+        product = firstNumber*secondNumber;
+
+        System.out.println("Difference is = "+difference);
+        System.out.println("Product is = "+product);
+
+    
+    }
+    
+}
